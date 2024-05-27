c35b810387b377c30a4d6e51debdc4c95585746d
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-05-27 15:00:48 +0200

    Inputs

--- NewTodoScreen.js
+++ NewTodoScreen.js
@@ -1,14 +1,24 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import { useState } from "react";
 
 export default function NewTodoScreen() {
+  const [titleInput, setTitleInput] = useState("");
+
   return (
     <View style={styles.container}>
-      <View>
-        <Text>Title</Text>
-      </View>
-      <View>
-        <Text>Description</Text>
+      <View style={styles.innerContainer}>
+        <View style={styles.titleContainer}>
+          <TextInput
+            style={styles.title}
+            placeholder="Title"
+            maxLength={28}
+            onChangeText={(titleInput) => setTitleInput(titleInput)}
+          />
+        </View>
+        <View style={styles.descriptionContainer}>
+          <Text>Description</Text>
+        </View>
       </View>
     </View>
   );
@@ -20,5 +30,33 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  innerContainer: {
+    flex: 0,
+    flexDirection: "column",
+    gap: 15,
+  },
+
+  titleContainer: {
+    width: 350,
+    borderColor: "#111111",
+    borderWidth: 2,
+  },
+
+  title: {
+    fontSize: 25,
+  },
+
+  descriptionContainer: {
+    width: 350,
+    height: 200,
+    borderColor: "#111111",
+    borderWidth: 2,
+    padding: 10, //problem
+  },
+
+  description: {
+    fontSize: 25,
   },
 });
